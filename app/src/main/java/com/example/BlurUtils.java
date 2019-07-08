87a19c76db21e80dd8e1391871de2a07ca168240
SecretYi <hjerz02@163.com>
2019-07-08 15:09:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/BlurUtils.java
@@ -0,0 +1,68 @@
+package com.example;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+import androidx.annotation.FloatRange;
+
+/**
+ * @author Secret
+ * @since 2019/7/8
+ */
+public class BlurUtils {
+
+    static {
+        System.loadLibrary("dk-blur-lib");
+    }
+
+    public static native void gaussBlur(Bitmap bitmap, int radius);
+
+    /**
+     * Not recommended
+     * <p>
+     * use renderScript to implement Gaussian Blur
+     *
+     * @param bitmap The original bitmap
+     * @param radius The radius of the blur
+     */
+    @Deprecated
+    public static Bitmap blurBitmap(Context context,Bitmap bitmap, @FloatRange(from = 0, to = 25, fromInclusive = false) float radius) {
+
+        //Let's create an empty bitmap with the same size of the bitmap we want to blur
+        Bitmap outBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Instantiate a new Renderscript
+        RenderScript rs = RenderScript.create(context);
+
+        //Create an Intrinsic Blur Script using the Renderscript
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+        //Create the Allocations (in/out) with the Renderscript and the in/out bitmaps
+        Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
+        Allocation allOut = Allocation.createFromBitmap(rs, outBitmap);
+
+        //Set the radius of the blur: 0 < radius <= 25
+        blurScript.setRadius(25.0f);
+
+        //Perform the Renderscript
+        blurScript.setInput(allIn);
+        blurScript.forEach(allOut);
+
+        //Copy the final bitmap created by the out Allocation to the outBitmap
+        allOut.copyTo(outBitmap);
+
+        //recycle the original bitmap
+        bitmap.recycle();
+
+        //After finishing everything, we destroy the Renderscript.
+        rs.destroy();
+
+        return outBitmap;
+
+    }
+
+}
